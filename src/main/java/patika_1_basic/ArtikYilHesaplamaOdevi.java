ea47f0dcc86d9af1e061a9624efccf2fad738676
MELEK CEVİZ <88192718+mlkcvz@users.noreply.github.com>
2025-03-06 09:25:19 +0300

    hesaplama yanlıştı , güncelledim

--- src/main/java/patika_1_basic/ArtikYilHesaplamaOdevi.java
+++ src/main/java/patika_1_basic/ArtikYilHesaplamaOdevi.java
@@ -8,12 +8,15 @@ public class ArtikYilHesaplamaOdevi
         Scanner scanner = new Scanner(System.in);
         System.out.println("Yıl bilgisini giriniz: ");
         int yil = scanner.nextInt();
-        if(yil%4==0  || yil%400==0)
-        {
-            System.out.println(yil + " Artık yıldır ! ");
+        if ((yil % 4 == 0 && yil % 100 != 0) || (yil % 400 == 0) ) {
+
+                System.out.println("artık yıldır");
+
+
         }
-        else {
-            System.out.println(yil + " Artık yıl degildir ! ");
+        else{
+            System.out.println(" artık yıl değildir ");
         }
+
     }
 }
